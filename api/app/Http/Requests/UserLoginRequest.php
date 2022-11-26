664de2896014e2b689d668a0e5579b8730949161
hotehama2022 <hotehama-r@cin-group.com>
2022-11-26 18:11:58 +0900

    ログイン機能実装

---
+++ api/app/Http/Requests/UserLoginRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use \Symfony\Component\HttpFoundation\Response;
+
+class UserLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => [
+                'required',
+                'exists:users,email'
+            ],
+            'password' => [
+                'required',
+                'exists:users,password'
+            ]
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'メールアドレスは必須項目です。',
+            'email.exists' => 'メールアドレスが違います。',
+            'password.required' => 'パスワードは必須項目です。',
+            'password.exists' => 'パスワードが違います。'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ], Response::HTTP_BAD_REQUEST);
+        throw new HttpResponseException($res);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/user/regist', [App\Http\Controllers\UserController::class, 'register']);
 
 Route::post('/user/register', [App\Http\Controllers\UserController::class, 'mainRegister']);
+
+Route::post('/user/login', [App\Http\Controllers\UserController::class, 'validateLogin']);
